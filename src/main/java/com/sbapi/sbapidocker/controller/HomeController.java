7ee355e45ff14c59d3baca649418b3048e5ae3f4
smileselva.sk@gmail.com <smileselva.sk@gmail.com>
2022-01-23 21:44:34 +0530

    second commit to push

--- src/main/java/com/sbapi/sbapidocker/controller/HomeController.java
+++ src/main/java/com/sbapi/sbapidocker/controller/HomeController.java
@@ -6,10 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 	
+		
 	@GetMapping("/home")
 	public String getHome()
 	{
 		return "Home Called";
+	}
+	
+	
+	@GetMapping("/message")
+	public String getMessage()
+	{
+		return "Home Called and method is getMessage";
 	}
 
 }
