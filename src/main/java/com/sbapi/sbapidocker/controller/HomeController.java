a9e169ccb228d9f2057f9d54fd86eddc5236597c
smileselva.sk@gmail.com <smileselva.sk@gmail.com>
2022-03-27 12:57:49 +0530

    Added WebFlux Configurations

--- src/main/java/com/sbapi/sbapidocker/controller/HomeController.java
+++ src/main/java/com/sbapi/sbapidocker/controller/HomeController.java
@@ -1,11 +1,22 @@
 package com.sbapi.sbapidocker.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sbapi.sbapidocker.client.APIClient;
+import com.sbapi.sbapidocker.model.JsonPlaceholderResponse;
+import com.sbapi.sbapidocker.service.HomeService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class HomeController {
 	
+	@Autowired
+	private HomeService homeservice;
 		
 	@GetMapping("/home")
 	public String getHome()
@@ -19,5 +30,16 @@ public class HomeController {
 	{
 		return "Home Called and method is getMessage";
 	}
+	
+	@GetMapping("/placeholder/{id}")
+	public Mono<JsonPlaceholderResponse> getJsonPlaceholder(@PathVariable ("id") String id)
+	{
+		return homeservice.getPlaceholderDataMono(id);
+	}
 
+	@GetMapping("/placeholders")
+	public Flux<JsonPlaceholderResponse> getJsonPlaceholderFlux()
+	{
+		return homeservice.getPlaceholderDataFlux();
+	}
 }
